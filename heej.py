0fc43caab8980443231cdae2c0330ac802e25ac5
heejin <sisesu92@gmail.com>
2021-02-27 10:57:07 +0900

    feat : custom error page 추가
    - 404(notfound), 403(forbidden), 500(internal server error)

--- heej.py
+++ heej.py
@@ -1,7 +1,37 @@
 import os
 from app import create_app, db
+from flask import render_template, request, jsonify
 
 app = create_app(os.environ.get("FLASK_CONFIG") or "default")
+
+@app.errorhandler(403)
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
+    return render_template('error/403.html'), 403
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
+    return render_template('error/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
+    return render_template('error/500.html'), 500
 
 
 if __name__ == "__main__":
